1c728483ce93b415170e9bfda886e1e07d372f1d
Michael Reilly <mreilly@bayareametro.gov>
2019-12-30 14:29:36 -0800

    Update development_projects.py

--- basemap/development_projects.py
+++ basemap/development_projects.py
@@ -1,2 +1,8 @@
 ﻿# coding: utf-8
 import arcpy
+
+target_features = "E:/baydata/smelt.gdb/cs1620"
+join_features = "E:/baydata/smelt.gdb/p10"
+out_feature_class = "E:/baydata/smelt.gdb/ttt_cs1620_p10"
+
+# arcpy.SpatialJoin_analysis(target_features, join_features, out_feature_class)
