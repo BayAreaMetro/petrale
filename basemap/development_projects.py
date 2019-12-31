0e5494757f243e7fd2f665a074b969348ca3ff0f
Michael Reilly <mreilly@bayareametro.gov>
2019-12-31 13:54:06 -0800

    Update development_projects.py

--- basemap/development_projects.py
+++ basemap/development_projects.py
@@ -18,6 +18,7 @@ arcpy.SpatialJoin_analysis(cs1620, p10, cs1620p10JOIN)
 
 
 # add fields and calc values
+# full list development_projects_id,raw_id,building_name,site_name,action,scen0,scen1,scen2,scen3,scen4,scen5,scen6,scen7,scen10,scen11,scen12,scen15,address,city,zip,county,x,y,geom_id,year_built,duration,building_type_id,building_type,building_sqft,non_residential_sqft,residential_units,unit_ave_sqft,tenure,rent_type,stories,parking_spaces,Average Weighted Rent,rent_ave_sqft,rent_ave_unit,last_sale_year,last_sale_price,source,edit_date,editor,version,
 
 arcpy.AddField_management(inFeatures, fieldName1, "DOUBLE", 
                           fieldPrecision, fieldScale)
