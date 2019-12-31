31850202811dd945acdcabc85993e4cb06f8d194
Michael Reilly <mreilly@bayareametro.gov>
2019-12-31 15:03:21 -0800

    Update development_projects.py

--- basemap/development_projects.py
+++ basemap/development_projects.py
@@ -36,6 +36,7 @@ arcpy.SpatialJoin_analysis(rfmu1619, p10, rfmu1619p10JOIN)
 
 
 # rename any conflicting field names
+# arcpy.AlterField_management(fc, field.name, 'ELEVATION', 'Elevation in Metres')
 
 # add fields and calc values
 # full list development_projects_id,raw_id,building_name,site_name,action,scen0,scen1,scen2,scen3,scen4,scen5,scen6,scen7,scen10,scen11,scen12,scen15,address,city,zip,county,x,y,geom_id,year_built,duration,building_type_id,building_type,building_sqft,non_residential_sqft,residential_units,unit_ave_sqft,tenure,rent_type,stories,parking_spaces,Average Weighted Rent,rent_ave_sqft,rent_ave_unit,last_sale_year,last_sale_price,source,edit_date,editor,version
