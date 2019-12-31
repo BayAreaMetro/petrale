f5fcf3d93af6b51e7e593c716d3155576c862d7f
Michael Reilly <mreilly@bayareametro.gov>
2019-12-31 13:40:17 -0800

    Update development_projects.py

--- basemap/development_projects.py
+++ basemap/development_projects.py
@@ -1,8 +1,28 @@
 ﻿# coding: utf-8
 import arcpy
 
+
+# set enviro
+
+# set vars
+
 cs1620 = "E:/baydata/smelt.gdb/cs1620"
 p10 = "E:/baydata/smelt.gdb/p10"
 cs1620p10JOIN = "E:/baydata/smelt.gdb/ttt_cs1620_p10"
 
+
+# spatial joins
+
 arcpy.SpatialJoin_analysis(cs1620, p10, cs1620p10JOIN)
+
+
+
+# add fields and calc values
+
+arcpy.AddField_management(inFeatures, fieldName1, "DOUBLE", 
+                          fieldPrecision, fieldScale)
+
+arcpy.AddField_management(inFeatures, fieldName1, "DOUBLE", 
+                          fieldPrecision, fieldScale)
+
+
