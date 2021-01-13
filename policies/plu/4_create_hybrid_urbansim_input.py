9843af717651cb2bcd881ca1a4772e2b6bbb6a39
Yuqi <ywang@bayareametro.gov>
2021-01-12 19:36:01 -0800

    optional code to fix overestimation of max_dua in hybrid base zoning

--- policies/plu/4_create_hybrid_urbansim_input.py
+++ policies/plu/4_create_hybrid_urbansim_input.py
@@ -31,6 +31,7 @@ PREV_ZONING_PARCELS_FILE    = os.path.join(M_DIR, 'Horizon', 'Large General Inpu
 PLU_BOC_M_DIR               = os.path.join(M_DIR, 'Final_Blueprint', 'Base zoning', 'output')
 PLU_BOC_FILE                = os.path.join(PLU_BOC_M_DIR, '2020_10_20_p10_plu_boc_allAttrs.csv')
 HYBRID_INDEX_DIR            = os.path.join(GITHUB_PETRALE_DIR, 'policies\\plu\\hybrid_index')
+PARCEL_UGB_TYPE_FILE        = os.path.join(M_DIR, 'Final_Blueprint', 'Zoning Modifications', 'parcels_UGB_type_FBP.csv')
 # TODO: change to idx_urbansim.csv when we have one
 HYBRID_INDEX_FILE           = os.path.join(HYBRID_INDEX_DIR, "idx_urbansim_fb_11052020.csv")
 
@@ -93,6 +94,24 @@ if __name__ == '__main__':
                               right= allowed_urbansim, 
                               on   = 'PARCEL_ID', 
                               how  = 'left')
+
+    # force max_dua_urbansim to 0 to avoid over-estimating max_dua during imputation
+    # two options: 
+    #   option 1: for parcels outside of UGB and don't allow residential development, force max_dua_urbansim to 0
+    #   option 2: for parcels that don't allow residential development, force max_dua_urbansim to 0
+    parcel_UBG_type = pd.read_csv(PARCEL_UGB_TYPE_FILE)
+    logger.info('Read {} lines from {}'.format(len(parcel_UBG_type), PARCEL_UGB_TYPE_FILE))
+    plu_boc_hybrid = pd.merge(left = plu_boc_hybrid,
+                              right= parcel_UBG_type,
+                              on   = 'PARCEL_ID',
+                              how  = 'left')
+    # option 1
+    # plu_boc_hybrid.loc[(plu_boc_hybrid.UGB_type == 'outside UGB') & (plu_boc_hybrid.allow_res_urbansim == 0),
+    #                    'max_dua_urbansim'] = 0
+
+    # option 2
+    plu_boc_hybrid.loc[plu_boc_hybrid.allow_res_urbansim == 0,
+                       'max_dua_urbansim'] = 0    
 
     logger.info('Saving hybrid zoning for {} parcels to {}'.format(len(plu_boc_hybrid),HYBRID_PARCELS_FILE))
     logger.debug('plu_boc_hybrid dtypes:\n{}'.format(plu_boc_hybrid.dtypes))
